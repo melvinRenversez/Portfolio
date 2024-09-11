702454d840ade8bb7636aacfe93944ecca9fe509
melvinRenversez <163991271+louanMel@users.noreply.github.com>
2024-09-11 19:07:01 +0200

    Message de votre commit

---
+++ contact.js
@@ -0,0 +1,24 @@
+// Initialisation d'EmailJS
+(function() {
+    emailjs.init("4wXxxOITTUo3oj8FB"); // Remplacez par votre User ID EmailJS
+})();
+
+// Gestion de la soumission du formulaire
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Empêche le rechargement de la page
+
+    // Paramètres EmailJS
+    const serviceID = 'service_bq3858t'; // Remplacez par votre service ID EmailJS
+    const templateID = 'template_semal8g'; // Remplacez par votre template ID EmailJS
+
+    // Envoi du formulaire via EmailJS
+    emailjs.sendForm(serviceID, templateID, this)
+        .then(() => {
+            alert('Email envoyé avec succès à melvin.renversez@ltpdampierre.fr!');
+        }, (err) => {
+            alert('L\'envoi de l\'email a échoué!\r\n Response:\n ' + JSON.stringify(err));
+        });
+
+    document.getElementById('contact-form').reset();
+
+});
