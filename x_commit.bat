b0cb86184b0b3a90c73e01d27758bc1c6992be0b
melvinRenversez <melvinrenversez003@gmail.com>
2024-09-17 09:19:56 +0200

    17/09/2024::09:19:56

--- x_commit.bat
+++ x_commit.bat
@@ -1,13 +1,26 @@
 @echo off
 git add .
 
+
+
 set currentDate=%date%
+set currentTime=%time%
+
 
 set day=%currentDate:~0,2%
 set month=%currentDate:~3,2%
 set year=%currentDate:~-4%
 
-git commit -m %day%/%month%/%year%
+
+set hour=%currentTime:~0,2%
+set minute=%currentTime:~3,2%
+set second=%currentTime:~6,2%
+
+
+if "%hour:~0,1%" == " " set hour=0%hour:~1,1%
+
+
+git commit -m %day%/%month%/%year%::%hour%:%minute%:%second%
 
 git push origin main
 
