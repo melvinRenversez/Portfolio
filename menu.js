b8f5ac00d855e04f51ebb97b302643d05dd902fc
melvinRenversez <melvinrenversez003@gmail.com>
2024-09-18 10:32:29 +0200

    18/09/2024::10:32:29

--- menu.js
+++ menu.js
@@ -9,13 +9,15 @@ var open = false
 menu.addEventListener('click', (e)=>{
     if(open){
         nav.classList.remove('actif')
+        menu.classList.remove('actif')
         closeSVG.classList.remove('actif')
         openSVG.classList.add('actif')
         open = false
     }else{
+        nav.classList.add('actif')
+        menu.classList.add('actif')
         openSVG.classList.remove('actif')
         closeSVG.classList.add('actif')
-        nav.classList.add('actif')
         open = true
     }
 })
