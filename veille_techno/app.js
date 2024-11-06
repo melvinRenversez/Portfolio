3f04c64d7853e3cab6f188588abff8905df735c7
melvinRenversez <melvinrenversez003@gmail.com>
2024-11-06 09:54:14 +0100

    Scroll

--- veille_techno/app.js
+++ veille_techno/app.js
@@ -1,6 +1,8 @@
 const colorSwitch = document.getElementById('colorSwitch')
 const circle = document.getElementById('circle')
 
+const scroll = document.getElementById('scroll')
+
 var actif = false
 
 colorSwitch.addEventListener('click', () => {
@@ -12,5 +14,25 @@ colorSwitch.addEventListener('click', () => {
         circle.classList.remove('actif')
         document.body.classList.remove('actif')
         actif = false
+    }
+})
+
+window.addEventListener('scroll', function() {
+    let scrollPosition = window.scrollY || document.documentElement.scrollTop;
+    if (scrollPosition > 0){
+        scroll.classList.add('top')
+        scroll.classList.remove('bottom')
+    }else{
+        scroll.classList.add('bottom')
+        scroll.classList.remove('top')
+    }
+}); 
+
+scroll.addEventListener('click', (e)=>{
+    var pos = scroll.classList[0]
+    if(pos == "bottom"){
+        window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
+    }else{
+        window.scrollTo({ top: 0, behavior: 'smooth' });
     }
 })
